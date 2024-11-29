1b96365172478d41b699147a6df4f47e3ff89943
Eric LLorente <ellorente@santelys.fr>
2024-11-29 15:56:09 +0100

    creation du composant pour debug les item

---
+++ src/lib/DebugItem.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	//extensions
+	import '$extensions/string.extension.js';
+
+	//utils
+	import { copyToClipboard } from './utils/clipboard';
+
+	//SMUI
+	import Button from '@smui/button';
+
+	export let items: any;
+</script>
+
+<Button
+	on:click={() => {
+		copyToClipboard(JSON.stringify(items, null, 2));
+	}}
+	>Copier
+</Button>
+
+<pre>
+	{JSON.stringify(items, null, 2)}        
+</pre>
