45b953c66441e0c2e423fa3d37638e5996078d2b
Eric LLorente <ellorente@santelys.fr>
2025-02-07 10:50:13 +0100

    corrections

--- src/lib/_404_.svelte
+++ src/lib/_404_.svelte
@@ -1,41 +1,40 @@
 <script>
-    export let message = "Attention ! Composant non trouvé.";
-  </script>
-  
-  <div class="attention-panel">
-    <div class="icon">⚠️</div>
-   <!--  <div class="text">{message}</div> -->
-  </div>
-  
-  <style>
-    .attention-panel {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-direction: column;
-      padding: 1.5em;
-      border: 2px solid #ffcc00;
-      border-radius: 8px;
-      background-color: #fffbe6;
-      color: #665c00;
-      font-size: 1.5em;
-      font-weight: bold;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-      max-width: 100%;
-      max-height: 100%;
-      min-width: 100%;
-      min-height: 100%;
-      margin: auto;
-      text-align: center;
-    }
-  
-    .icon {
-      font-size: 2em;
-      margin-bottom: 0.5em;
-    }
-  
-    .text {
-      font-size: 1.2em;
-    }
-  </style>
-  
+	export let message = 'Attention ! Composant non trouvé.';
+</script>
+
+<div class="attention-panel">
+	<div class="icon">⚠️</div>
+	<!--  <div class="text">{message}</div> -->
+</div>
+
+<style>
+	.attention-panel {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		padding: 1.5em;
+		border: 2px solid #ffcc00;
+		border-radius: 8px;
+		background-color: #fffbe6;
+		color: #665c00;
+		font-size: 1.5em;
+		font-weight: bold;
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+		max-width: 100%;
+		max-height: 100%;
+		min-width: 100%;
+		min-height: 100%;
+		margin: auto;
+		text-align: center;
+	}
+
+	.icon {
+		font-size: 2em;
+		margin-bottom: 0.5em;
+	}
+
+	.text {
+		font-size: 1.2em;
+	}
+</style>
--- src/routes/examples/gridPage/componentsMap.ts
+++ src/routes/examples/gridPage/componentsMap.ts
@@ -1,13 +1,13 @@
 //src/routes/examples/gridPage/componentMap.ts
-import HelloWorld from "$lib/examples/components/showHelloWorld.svelte";
-import ByeByeWorld from "$lib/examples/components/showByeByeWorld.svelte";
-import type { ComponentsMap } from "$lib";
+import HelloWorld from '$lib/examples/components/showHelloWorld.svelte';
+import ByeByeWorld from '$lib/examples/components/showByeByeWorld.svelte';
+import type { ComponentsMap } from '$lib';
 // Importez ici tous les autres composants nécessaires
 
 export const componentsMap: ComponentsMap = {
-  HelloWorld: HelloWorld,
-  ByeByeWorld: ByeByeWorld,
-  // Ajoutez d'autres composants ici
+	HelloWorld: HelloWorld,
+	ByeByeWorld: ByeByeWorld
+	// Ajoutez d'autres composants ici
 };
 
 export default componentsMap;
